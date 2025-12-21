6c9ba0d0f03ab9861e9b46c1b32f1d9f00a41167
root <root@cloud-server-0>
2025-12-21 20:40:21 +0000

    simple fix to audio_http_service bind

--- src/services/audio_http_service.rs
+++ src/services/audio_http_service.rs
@@ -10,15 +10,24 @@ pub struct AudioHttpService {
 
 impl AudioHttpService {
     pub fn new(bind: impl Into<String>) -> Self {
-        Self { bind: bind.into() }
+        Self {
+            bind: bind.into(),
+        }
     }
 
     pub fn start(&self, wav_dir: PathBuf, ring: AudioRing) {
+        // eine Kopie für Logs / Außenwelt
         let bind = self.bind.clone();
+        // eine Kopie für den Worker-Thread
+        let bind_for_thread = bind.clone();
 
         std::thread::spawn(move || {
-            if let Err(e) = start_audio_http_server(&bind, wav_dir, move || ring.subscribe()) {
-                error!("[audio] HTTP server failed: {}", e);
+            if let Err(e) = start_audio_http_server(
+                &bind_for_thread,
+                wav_dir,
+                move || ring.subscribe(),
+            ) {
+                error!("[audio_http] server failed: {}", e);
             }
         });
 
